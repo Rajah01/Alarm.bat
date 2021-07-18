e8c8c4c97128d257bebca1c54841892111bc0e63
Rajah01 <59131779+Rajah01@users.noreply.github.com>
2021-07-18 08:44:02 -0400

    Add files via upload

--- TestForShortDirectoryNames.bat
+++ TestForShortDirectoryNames.bat
@@ -1,21 +1,17 @@
 @echo off
-setlocal
-set cnt=0
-echo.
-for /F "skip=4 tokens=1 delims=~" %%A in ('dir /X "%SystemDrive%\Program Files*"') do call :a "%%A"
-goto :eof
-
+setlocal enableDelayedExpansion
+set fn="X"&set er=0
+@echo/
+for /F "skip=4 tokens=1 delims=~" %%A in ('dir /X "%SystemDrive%\Program Files*"') do if !fn!=="X" call :a "%%A"
+exit /B %er%
 :a
-if %cnt%==1 goto :eof
-set cnt=1
 set fn=%1
-set fn=%fn:~-7,6%
-if "%fn%" NEQ "PROGRA" (
-echo POTENTIALLY FATAL ERROR: Your system does not support Short "8.3" directory names.
-echo Make SURE critical files are in locations with NO spaces in the "drive:\path\" dir tree:
-echo.
-echo 	Alarm.bat and bell.exe
-echo 	%%TEMP%% files are in "%TEMP%\ALRM\"
-echo 	Most executables are in "%SystemRoot%\System32\"
-) else echo Everything is OK!
+if "%fn:~-7,6%"=="PROGRA" @echo 	Everything is OK!&goto :eof
+set er=1
+@echo POTENTIALLY FATAL ERROR: Your system does not support Short "8.3" directory names.
+@echo Make SURE critical files are in locations with NO spaces in the "drive:\path\" dir tree:
+@echo/
+@echo 	Alarm.bat and bell.exe
+@echo 	%%TEMP%% files are in "%TEMP%\ALRM\"
+@echo 	Most executables are in "%SystemRoot%\System32\"
 goto :eof
