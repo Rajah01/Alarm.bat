7fec0137bbd7af555abf56d023e4c3aeda96eced
Rajah01 <59131779+Rajah01@users.noreply.github.com>
2021-07-18 10:17:25 -0400

    Add files via upload

--- TestForShortDirectoryNames.bat
+++ TestForShortDirectoryNames.bat
@@ -1,12 +1,11 @@
 @echo off
 setlocal enableDelayedExpansion
-set fn="X"&set er=0
+set fn="#"&set er=0
 @echo/
-for /F "skip=4 tokens=1 delims=~" %%A in ('dir /X "%SystemDrive%\Program Files*"') do if !fn!=="X" call :a "%%A"
-exit /B %er%
+for /F "skip=4 tokens=1,* delims=~" %%A in ('dir /X "%SystemDrive%\Program Files*"') do if !fn! NEQ "#" (exit /B !er!) else call :a "%%A" "%%B"
 :a
 set fn=%1
-if "%fn:~-7,6%"=="PROGRA" @echo 	Everything is OK!&goto :eof
+if "%~2#" NEQ "#" if "%fn:~-7,6%"=="PROGRA" @echo 	Everything is OK!&goto :eof
 set er=1
 @echo POTENTIALLY FATAL ERROR: Your system does not support Short "8.3" directory names.
 @echo Make SURE critical files are in locations with NO spaces in the "drive:\path\" dir tree:
